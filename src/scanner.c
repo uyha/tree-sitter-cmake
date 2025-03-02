def189b2d9395d8ebfe946ff7b1765dc8899a971
Uy Ha <hchanuy@gmail.com>
2025-03-02 10:09:00 +0000

    chore: format `scanner.c`

--- src/scanner.c
+++ src/scanner.c
@@ -3,9 +3,13 @@
 
 enum TokenType { BRACKET_ARGUMENT, BRACKET_COMMENT, LINE_COMMENT };
 
-static void skip(TSLexer *lexer) { lexer->advance(lexer, true); }
+static void skip(TSLexer *lexer) {
+  lexer->advance(lexer, true);
+}
 
-static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
+static void advance(TSLexer *lexer) {
+  lexer->advance(lexer, false);
+}
 
 static void skip_wspace(TSLexer *lexer) {
   while (iswspace(lexer->lookahead)) {
@@ -58,8 +62,7 @@ static bool scan(UNUSED void *payload, TSLexer *lexer, bool const *valid_symbols
       return true;
     }
   }
-  if (lexer->lookahead == '#' &&
-      (valid_symbols[BRACKET_COMMENT] || valid_symbols[LINE_COMMENT])) {
+  if (lexer->lookahead == '#' && (valid_symbols[BRACKET_COMMENT] || valid_symbols[LINE_COMMENT])) {
     advance(lexer);
     if (is_bracket_argument(lexer)) {
       lexer->result_symbol = BRACKET_COMMENT;
@@ -76,12 +79,13 @@ static bool scan(UNUSED void *payload, TSLexer *lexer, bool const *valid_symbols
   return false;
 }
 
-void *tree_sitter_cmake_external_scanner_create() { return NULL; }
+void *tree_sitter_cmake_external_scanner_create() {
+  return NULL;
+}
 
 void tree_sitter_cmake_external_scanner_destroy(UNUSED void *payload) {}
 
-unsigned tree_sitter_cmake_external_scanner_serialize(UNUSED void *payload,
-                                                      UNUSED char *buffer) {
+unsigned tree_sitter_cmake_external_scanner_serialize(UNUSED void *payload, UNUSED char *buffer) {
   return 0;
 }
 
@@ -89,7 +93,6 @@ void tree_sitter_cmake_external_scanner_deserialize(UNUSED void *payload,
                                                     UNUSED char const *buffer,
                                                     UNUSED unsigned length) {}
 
-bool tree_sitter_cmake_external_scanner_scan(void *payload, TSLexer *lexer,
-                                             bool const *valid_symbols) {
+bool tree_sitter_cmake_external_scanner_scan(void *payload, TSLexer *lexer, bool const *valid_symbols) {
   return scan(payload, lexer, valid_symbols);
 }
