af17374abe0b3329f4ce6166f943f870ba2332eb
Uy Ha <hchanuy@gmail.com>
2025-05-23 08:19:42 +0000

    generate with ABI 14

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 832
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 96
@@ -2704,7 +2704,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 19, .external_lex_state = 2},
   [2] = {.lex_state = 13, .external_lex_state = 2},
@@ -20083,7 +20083,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_cmake(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -20107,13 +20106,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_cmake(void) {
       tree_sitter_cmake_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "cmake",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 7,
-      .patch_version = 0,
-    },
   };
   return &language;
 }
